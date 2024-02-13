64a979a8a48fb892ae2df59157312d4a7639f80b
Brandon <bapuntar@ucsc.edu>
2024-02-13 15:59:47 -0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,17 @@
+// Name: Brandon Apuntar
+// hours spent:
+//
+
+const config = {
+    parent: 'phaser-game',
+    type: Phaser.WEBGL,
+    width: 600,
+    height: 500,
+    pixelArt: true,
+    physics: {
+        default: "arcade",
+        arcade: {
+            debug: true
+        }
+    }
+}
